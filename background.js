9a74569229f05d7c8a20b80082b569839d622dd2
jeonsol <jeonsol@nts-corp.com>
2023-06-09 15:55:13 +0900

    refactoring

--- background.js
+++ background.js
@@ -1,7 +1,13 @@
-
 const sortByLike = () => {
-  const isYoutube = window.location.hostname + window.location.pathname === 'www.youtube.com/watch';
-  if (!isYoutube) {
+  const PAGE_CONTAINER_SELECTOR = '#content'
+  const COMMENT_CONTAINER_SELECTOR = 'ytd-comments#comments #contents'
+  const COMMENT_SELECTOR = 'ytd-comment-thread-renderer'
+  const LIKE_COUNT_SELECTOR = '#vote-count-middle'
+
+  const YOUTUBE_WATCH_URL = 'www.youtube.com/watch'
+
+  const isYoutubeWatchPage = `${window.location.hostname}${window.location.pathname}` === YOUTUBE_WATCH_URL;
+  if (!isYoutubeWatchPage) {
     alert('유튜브 재생 페이지에서 사용해주세요.');
     return;
   }
@@ -9,13 +15,14 @@ const sortByLike = () => {
   const findCommentContainer = new Promise((resolve) => {
     let id;
     const animation = () => {
-      const commentContainer = document.querySelector('ytd-comments#comments #contents');
+      const commentContainer = document.querySelector(COMMENT_CONTAINER_SELECTOR);
 
       if (commentContainer) {
         cancelAnimationFrame(id);
-        resolve();
+        resolve(commentContainer);
       } else {
-        const contentHeight = document.querySelector('#content').scrollHeight;
+        const contentHeight = document.querySelector(PAGE_CONTAINER_SELECTOR).scrollHeight;
+
         window.scrollTo(0, contentHeight);
         window.requestAnimationFrame(animation);
       }
@@ -23,35 +30,38 @@ const sortByLike = () => {
     id = window.requestAnimationFrame(animation);
   })
 
-  const findAllComments = new Promise((resolve) => {
+  const findAllCommentList = new Promise((resolve) => {
     const commentLengthList = [];
 
     let id = setInterval(() => {
-      const contentHeight = document.querySelector('#content').scrollHeight;
-      const comments = document.querySelectorAll('ytd-comment-thread-renderer');
-      window.scrollTo(0, contentHeight);
-      commentLengthList.push(comments.length);
+      const contentHeight = document.querySelector(PAGE_CONTAINER_SELECTOR).scrollHeight;
+      const commentList = document.querySelectorAll(COMMENT_SELECTOR);
 
-      const length = commentLengthList.length;
-      if (commentLengthList[length - 1] === commentLengthList[length - 2] && commentLengthList[length - 2] === commentLengthList[length - 3]) {
+      window.scrollTo(0, contentHeight);
+      commentLengthList.push(commentList.length);
+
+      const {length} = commentLengthList;
+      const isScrollEnd = commentLengthList[length - 1] === commentLengthList[length - 2] && commentLengthList[length - 2] === commentLengthList[length - 3]
+
+      if (isScrollEnd) {
         clearInterval(id);
         resolve();
       } else {
-        console.log(`${comments.length}개의 댓글 파싱 중..`)
+        console.log(`${commentList.length}개의 댓글 파싱 중..`)
       }
     }, 1000);
   })
 
-  Promise.all([findCommentContainer, findAllComments]).then(() => {
-    const commentContainer = document.querySelector('ytd-comments#comments #contents');
-    const comments = document.querySelectorAll('ytd-comment-thread-renderer');
-    const orderByLike = Array.from(comments).sort((a, b) => {
-      return Number(b.querySelector('#vote-count-middle').textContent) - Number(a.querySelector('#vote-count-middle').textContent);
+  Promise.all([findCommentContainer, findAllCommentList]).then(() => {
+    const commentContainer = document.querySelector(COMMENT_CONTAINER_SELECTOR);
+    const commentList = document.querySelectorAll(COMMENT_SELECTOR);
+    const orderByLike = Array.from(commentList).sort((a, b) => {
+      return Number(b.querySelector(LIKE_COUNT_SELECTOR).textContent) - Number(a.querySelector(LIKE_COUNT_SELECTOR).textContent);
     })
     commentContainer.innerHTML = '';
     orderByLike.forEach(item => commentContainer.appendChild(item));
 
-    window.scrollTo(0, 2920);
+    commentContainer.scrollIntoView();
     console.log('----*****댓글 좋아요 순으로 정렬 완료*****----')
     alert('댓글 정렬 완료')
   })

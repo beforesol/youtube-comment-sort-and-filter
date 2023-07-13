4d67187c69563017b8371aa9530bd955ae99691e
jeonsol <jeonsol12@gmail.com>
2023-07-13 15:35:56 +0900

    시간 정규식 수정

--- js/contants/regex.js
+++ js/contants/regex.js
@@ -3,4 +3,4 @@ export const EN_REGEX = /[^A-Za-z]/g
 export const ZH_CHS_REGEX = /[^\u4E00-\u9FFF\u3400-\u4DBF]/g
 export const JA_REGEX = /[^\u3040-\u309F\u30A0-\u30FF\u31F0-\u31FF\uFF65-\uFF9F]/g
 export const TH_REGEX = /[^\u0E00-\u0E7F]/g
-export const TIME_REGEX = /\b\d{1,2}:\d{2}:\d{2}\b/;
+export const TIME_REGEX = /\b(?:\d{1,2}:)?\d{1,2}:\d{1,2}\b/;
